e2d02b84e48fb7d214a9f9ea21428675234a5a5e
jw <web_Jiwei@163.com>
2020-06-16 15:09:10 +0800

    更新内容

--- src/view/Login/index.js
+++ src/view/Login/index.js
@@ -4,12 +4,16 @@ import NavHeader from '../../components/NavHeader';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import session from '../../store/actions/session'
+import Alert from '../../components/Alert'
+
 class Login extends Component {
   onLogin = () => {
     const { login } = this.props;
     login({ username: this.phone.value, password: this.password.value })
   }
   render() {
+    const alert = <Alert type={this.props.success ? 'success' : 'error'} message={this.props.success || this.props.error} />
+
     return (
       <div className='login'>
         <NavHeader title='登录'></NavHeader>
@@ -20,6 +24,9 @@ class Login extends Component {
         <input type='password' placeholder='密码' ref={ref => this.password = ref}></input>
         <Link to='/reg'>前往注册</Link>
         <button onClick={this.onLogin}>登&nbsp;&nbsp;录</button>
+        {
+          this.props.success || this.props.error ? alert : ""
+        }
       </div>
     )
   }
